ad20eb0ccae96fdaa15a1f03e1c7d834c79563e5
chanaelstar <chanaelstar@gmail.com>
2026-01-26 15:48:00 +0100

    add card

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<script setup></script>
+<script setup>
+import FossilCard from './components/card.vue';
+
+import { fetchFossils } from './services/api.js';
+fetchFossils();
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <FossilCard />
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
