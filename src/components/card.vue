69d49421b527dcac51b28c3b7bdc06df7d3fc95c
chanaelstar <chanaelstar@gmail.com>
2026-01-26 13:56:07 +0100

    components

---
+++ src/components/card.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Fossils</h1>
+  <div id="fossil-gallery">
+    <FossilCard />
+    <FossilCard />
+    <FossilCard />
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+#fossil-gallery {
+  display: flex;
+  flex: wrap;
+}
+</style>
