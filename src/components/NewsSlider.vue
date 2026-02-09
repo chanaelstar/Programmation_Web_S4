b89e84b28821f9cf75e5ec3d51de1ea66185fd69
chanaelstar <chanaelstar@gmail.com>
2026-02-09 15:49:35 +0100

    call acnh api

---
+++ src/components/NewsSlider.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, computed, watch } from 'vue'
+import { getfossilData } from '@/services/api'
+import card from './components/card.vue'
+import NewsSlider from './components/NewsSlider.vue'
+
+const fossilData = ref([])
+const getData = async () => {
+    fossilData.value = await getfossilData()
+}
+getData()
+
+
+</script>
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,25 @@
-function fetchFossils() {
-    console.log("Fetching fossils data...");
+// function fetchFossils() {
+//     console.log("Fetching fossils data...");
+// }
+// export { fetchFossils };
+
+async function fetchFossilsGroup(){
+    try {
+        const response = await fetch('https://api.nookipedia.com/nh/fossils/groups', {
+            headers : {
+                'X-API-KEY': 'ba84654c-2920-44b3-8643-174e5114a9bc',
+                'Accept-Version': '1.0.0',
+            }
+        })
+        if (response.status == 200){
+            const data = await response.json()
+            console.log(data)
+            return data
+        }else {           
+             throw new Error (response.statusText)
+        }
+    } catch (error) {
+        console.error('Error fetching fossils group:', error)
 }
-export { fetchFossils };
+}
+
